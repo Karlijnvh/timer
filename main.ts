e9eda29a6ffc799e46627c8ef19b400877184e33
Karlijnvh <112547700+Karlijnvh@users.noreply.github.com>
2024-03-04 14:18:15 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
 let toestand = 1
 let timerEindtijd = input.runningTime() + 1000
 basic.forever(function () {
-	
+    if (true) {
+        if (true) {
+        	
+        }
+    }
+    if (true) {
+    	
+    }
 })
