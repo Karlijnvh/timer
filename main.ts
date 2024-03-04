4a08a0a5e356ea6af06c6b647c037f8b51b721d6
Karlijnvh <112547700+Karlijnvh@users.noreply.github.com>
2024-03-04 14:25:34 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,12 +1,24 @@
 let toestand = 1
-let timerEindtijd = input.runningTime() + 1000
+let timerEindtijd = input.runningTime() + 5000
 basic.forever(function () {
-    if (true) {
-        if (true) {
-        	
+    if (toestand == 1) {
+        if (input.runningTime() >= timerEindtijd) {
+            basic.showIcon(IconNames.Heart)
+            timerEindtijd = input.runningTime() + 5000
+            toestand = 2
         }
     }
-    if (true) {
-    	
+    if (toestand == 2) {
+        if (input.runningTime() >= timerEindtijd) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+            timerEindtijd = input.runningTime() + 5000
+            toestand = 1
+        }
     }
 })
